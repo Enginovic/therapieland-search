2681dda9d634ede5679ffed89f05542841f9a3d7
Engin Sasmaz <engins.93@gmail.com>
2021-05-18 21:37:08 +0200

    Added basic test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,23 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
+describe("Check for h1", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js + TypeScript App");
+    cy.contains("h1", "Search for a posts");
+  });
+});
+
+describe("Check for post items", () => {
+  it("starts with zero items", () => {
+    cy.visit("/");
+    cy.contains(".posts").should("not.exist");
+    cy.contains("h3", "No posts found");
+  });
+});
+
+describe("Check for history", () => {
+  it("starts with zero items", () => {
+    cy.visit("/");
+    cy.contains(".search-history").should("not.exist");
   });
 });
